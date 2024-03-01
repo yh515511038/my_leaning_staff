54852511cede6f114a8356a3c0504bd87951e083
yh515511038 <yh515511038@gmail.com>
2024-03-01 08:02:31 +0800

    add new flaskblog

--- myFirstFlask/flaskblog/flaskblog/__init__.py
+++ myFirstFlask/flaskblog/flaskblog/__init__.py
@@ -1,17 +1,42 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flaskblog.config import Config
 
 
-db = SQLAlchemy()
-
-
-def create_app(conig_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    db.init_app(app)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
     
+    
+
+    app.config.from_mapping(
+        SECRET_KEY='0ba857871f387b5a5239b3b15b0f3f0365d0ef7fb1958cdd89431ce298e75d82',
+        # SQLALCHEMY_DATABASE_URI='sqlite:///site.sqlite',
+        DATABASE=os.path.join(app.instance_path, 'site.sqlite')
+    )
+    
+    # if not test_config:
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     app.config.from_mapping(test_config)
+        
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except:
+        pass
+        
+    from . import db
+    db.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
     app.register_blueprint(main)
     
     return app
+
+
+app = create_app()
--- myFirstFlask/flaskblog/flaskblog/main/forms.py
+++ myFirstFlask/flaskblog/flaskblog/main/forms.py
@@ -1,23 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, validators
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flaskblog.main.models import User
+from wtforms import StringField, EmailField, PasswordField, SubmitField, validators
+from wtforms.validators import Length, EqualTo, Email
 
 
-class RegistrationForm(FlaskForm):
-  username = StringField('Username', DataRequired(), Length(min=2, max=50))
-  email = StringField('Email', DataRequired(), Length(min=2, max=100))
-  password = PasswordField('Password', DataRequired(), Length(min=5, max=100))
-  confirm_password = PasswordField('Confirm password', DataRequired(), EqualTo('password'))
-  submit = SubmitField('Sign Up')
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[Length(min=2, max=50)])
+    email = EmailField("Email", validators=[Email()])
+    password = PasswordField("Password", validators=[Length(min=5, max=50)])
+    confirm_password = PasswordField("Confirm Password", validators=[EqualTo("password")])
+    submit = SubmitField('Submit')
 
-  def validate_username(self, username):
-    user = User.query.filter_by(username=username.data)
-    if user:
-      return ValidationError('Username already exist, please choose another.')
-    
 
 class LoginForm(FlaskForm):
-  username = StringField('Username', DataRequired(), Length(min=2, max=50))
-  password = PasswordField('Password', DataRequired(), Length(min=2, max=5))
-  submit = SubmitField('Sign In')
+    username = StringField("Username", validators=[Length(min=2, max=50)])
+    password = PasswordField("Password", validators=[Length(min=5, max=50)])
+    submit = SubmitField('Submit')
+
